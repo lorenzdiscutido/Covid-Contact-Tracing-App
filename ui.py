999105077b3e67c11adf852d20f2b4b1d2b0e9a1
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-15 11:08:58 +0800

    Succesfully created the main window

--- ui.py
+++ ui.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 
 # Create the main window
+top = tk.Tk()
+top.title("Covid Tracing App")
+top.geometry("300x400")
 # Create a label
 # Create an entry field
 # Create a button
 # Start the main event loop
-
+top.mainloop()
