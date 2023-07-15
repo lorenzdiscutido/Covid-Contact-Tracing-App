4017612976125c1b08cebcdc8b5abe1e89ff803a
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-15 12:07:51 +0800

    Added labels for the data collection

--- ui.py
+++ ui.py
@@ -3,8 +3,16 @@ import tkinter as tk
 # Create the main window
 top = tk.Tk()
 top.title("Covid Tracing App")
-top.geometry("300x400")
+top.geometry("500x300")
 # Create a label
+name = tk.Label(top, text = "Full Name:")
+name.place(x=50, y=50)
+phone_number = tk.Label(top, text = "Phone Number:")
+phone_number.place(x=50, y=70)
+temperature = tk.Label(top, text = "Enter your temperature:")
+temperature.place(x=50, y=90)
+location = tk.Label(top, text = "Location of your visit:")
+date = tk.Label(top, text = "Date of your visit:")
 # Create an entry field
 # Create a button
 # Start the main event loop
