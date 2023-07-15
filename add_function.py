077406afd465d1050f924decdcfc58b621a67f78
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-15 15:23:31 +0800

    Made the add_function a class so the code can have a window forchoosing between adding or searching entry

---
+++ add_function.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+
+class add_function():
+    def __init__(self):
+        # Create a label
+        name = tk.Label(self, text = "Full Name:")
+        name.place(x=50, y=50)
+
+        phone_number = tk.Label(self, text = "Phone Number:")
+        phone_number.place(x=50, y=80)
+
+        temperature = tk.Label(self, text = "Enter your temperature:")
+        temperature.place(x=50, y=110)
+
+        location = tk.Label(self, text = "Location of your visit:")
+        location.place(x=50, y=140)
+
+        date = tk.Label(self, text = "Date of your visit(MM/DD/YY):")
+        date.place(x=50, y=170)
+
+        # Create an entry field
+        name_entry = tk.Entry(self)
+        name_entry.place(x=120, y=50)
+
+        number_entry = tk.Entry(self)
+        number_entry.place(x=150, y=80)
+
+        temp_entry = tk.Entry(self)
+        temp_entry.place(x=190, y=110)
+
+        location_entry = tk.Entry(self)
+        location_entry.place(x=180, y=140)
+
+        date_entry = tk.Entry(self)
+        date_entry.place(x=225, y=170)
+        # Create a button
+        # Start the main event loop
+        self.mainloop()
---
+++ contact_tracing_app.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from add_function import add_function
+
+class front_page():
+    def front_page():
+        front = tk.Tk()
+        front.title("Covid Tracing app")
+        front.geometry("500x300")
