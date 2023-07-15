4c2e5c5af21f56628fa67941f2caa99f8584f37b
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-15 12:12:26 +0800

    renamed the main window and added the labels for locatiob and date

--- ui.py
+++ ui.py
@@ -1,19 +1,21 @@
 import tkinter as tk
 
 # Create the main window
-top = tk.Tk()
-top.title("Covid Tracing App")
-top.geometry("500x300")
+info = tk.Tk()
+info.title("Covid Tracing App")
+info.geometry("500x300")
 # Create a label
-name = tk.Label(top, text = "Full Name:")
+name = tk.Label(info, text = "Full Name:")
 name.place(x=50, y=50)
-phone_number = tk.Label(top, text = "Phone Number:")
+phone_number = tk.Label(info, text = "Phone Number:")
 phone_number.place(x=50, y=70)
-temperature = tk.Label(top, text = "Enter your temperature:")
+temperature = tk.Label(info, text = "Enter your temperature:")
 temperature.place(x=50, y=90)
-location = tk.Label(top, text = "Location of your visit:")
-date = tk.Label(top, text = "Date of your visit:")
+location = tk.Label(info, text = "Location of your visit:")
+location.place(x=50, y=110)
+date = tk.Label(info, text = "Date of your visit:")
+date.place(x=50, y=130)
 # Create an entry field
 # Create a button
 # Start the main event loop
-top.mainloop()
+info.mainloop()
